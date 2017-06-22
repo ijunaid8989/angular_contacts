ee4a42a42e661f3628ea9164ac3d8a69cc955a4d
Junaid Farooq <junaid@evercam.io>
2017-06-22 06:05:34 +0500

    Added AngularResource and Factories.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express"),
     app = express()
+    api = require("./api")
     path = require("path");
 
 app
   .use(express.static('./public'))
+  .use("/api", api)
   .get("*", function(req, res){
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
   })
